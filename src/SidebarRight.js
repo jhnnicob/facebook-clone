cbdf9c0b2b6c6c982bff91e610e9d0c9368ac35a
jhnnicob <johnnicomolon@yahoo.com>
2021-08-07 15:14:26 +0800

    Implement Event and FriendRequest component

--- src/SidebarRight.js
+++ src/SidebarRight.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './SidebarRight.css';
+import Event from './event'
+import FriendRequest from './friendRequest';
 function SidebarRight() {
     return (
         <div className="sidebarRight">
-            
+            <Event />
+            <FriendRequest />
         </div>
     )
 }
