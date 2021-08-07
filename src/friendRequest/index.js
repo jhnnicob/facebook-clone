bb33694ee16a1c62534f1b54afc71068d3e8be27
jhnnicob <johnnicomolon@yahoo.com>
2021-08-07 15:04:57 +0800

    Add FriendRequestList component

---
+++ src/friendRequest/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './friendRequest.css';
+import FriendRequestList from '../friendRequestList';
+
+function FriendRequest() {
+    return (
+        <div className="friendRequest">
+            <FriendRequestList />
+        </div>
+    )
+}
+
+export default FriendRequest;
